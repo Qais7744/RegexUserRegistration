3d4ddc97af549bdb87069a6954abe2f6bc05b8b7
Qais7744 <90202329+Qais7744@users.noreply.github.com>
2021-10-27 03:27:24 +0530

    Update userRegistration.java

--- src/main/java/com/bl/userRegistration/userRegistration.java
+++ src/main/java/com/bl/userRegistration/userRegistration.java
@@ -3,7 +3,7 @@ package com.bl.userRegistration;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class userRegistration {
+public class UserRegistration {
 
     public boolean lastName() {
         boolean matchesString = Pattern.matches("^[A-Z]{1}[a-zA-Z]{3,10}$", "Bagwan");
