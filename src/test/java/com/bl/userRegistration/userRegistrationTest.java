115b489ff7d8b1b6ddff490a5b9a32c4e0b391c9
Qais7744 <90202329+Qais7744@users.noreply.github.com>
2021-10-27 03:32:00 +0530

    Update userRegistrationTest.java

--- src/test/java/com/bl/userRegistration/userRegistrationTest.java
+++ src/test/java/com/bl/userRegistration/userRegistrationTest.java
@@ -3,12 +3,12 @@ package com.bl.userRegistration;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class userRegistrationTest {
+public class UserRegistrationTest {
     private Assertions Assert;
 
     @Test
     public void givenPassword_WhenPreDefined_ShouldReturnTrue() {
-        userRegistration userRegistration = new userRegistration();
+        UserRegistration userRegistration = new UserRegistration();
         boolean userTest = userRegistration.password();
         Assert.assertTrue(userTest);
     }
