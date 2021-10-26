00afa8314b1383f95986590e3c33173eb33f9c10
Qais7744 <90202329+Qais7744@users.noreply.github.com>
2021-10-27 03:29:24 +0530

    Update userRegistration.java

--- src/main/java/com/bl/userRegistration/userRegistration.java
+++ src/main/java/com/bl/userRegistration/userRegistration.java
@@ -3,7 +3,7 @@ package com.bl.userRegistration;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class userRegistration {
+public class UserRegistration {
 
     public boolean phoneNumber() {
         boolean matchesString = Pattern.matches("^[0-9]{2}[ ][789]{1}[0-9]{9}$", "91 7744918893");
