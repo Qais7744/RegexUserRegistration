273cbd0d0cd9194960bb96202f0993a796db2dbe
unknown <altamashbagwan1@gmail.com>
2021-10-27 02:03:46 +0530

    Added: Fuctioning of Enter a Password for minimum eight character and one Numeric in userRegistration

--- src/main/java/com/bl/userRegistration/userRegistration.java
+++ src/main/java/com/bl/userRegistration/userRegistration.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class userRegistration {
 
     public boolean passwordUpperCase() {
-        boolean matchesString = Pattern.matches("^[A-Z{1}a-z]{8}$", "aLtamash");
+        boolean matchesString = Pattern.matches("^[A-Z{1}0-9{1}a-z]{8}$", "Al9amash");
         return matchesString;
     }
 }
