020b0aa49d61f63e87fdd0793748e8bcad6cd71b
Qais7744 <90202329+Qais7744@users.noreply.github.com>
2021-10-27 03:26:25 +0530

    Update userRegistrationTest.java

--- src/test/java/com/bl/userRegistration/userRegistrationTest.java
+++ src/test/java/com/bl/userRegistration/userRegistrationTest.java
@@ -3,12 +3,12 @@ package com.bl.userRegistration;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class userRegistrationTest {
+public class UserRegistrationTest {
     private Assertions Assert;
 
     @Test
     public void givenFirstNameWhenShouldReturnTrue() {
-        userRegistration userRegistration = new userRegistration();
+        UserRegistration userRegistration = new UserRegistration();
         boolean userTest = userRegistration.firstName();
         Assert.assertTrue(userTest);
     }
