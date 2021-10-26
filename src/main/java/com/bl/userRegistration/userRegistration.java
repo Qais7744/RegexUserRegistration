b503b56eebe33bdafa606b7089adfc0dbe0bd007
Qais7744 <90202329+Qais7744@users.noreply.github.com>
2021-10-27 03:28:51 +0530

    Update userRegistration.java

--- src/main/java/com/bl/userRegistration/userRegistration.java
+++ src/main/java/com/bl/userRegistration/userRegistration.java
@@ -3,7 +3,7 @@ package com.bl.userRegistration;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class userRegistration {
+public class UserRegistration {
 
     public boolean email() {
         boolean matchesString = Pattern.matches("^[A-Za-z._]+[@]{1}[a-zA-Z]+[.a-z]{4}([.a-zA-Z]{3})*$", "altamashbagwan.xyz@qais.com.in");
