0169d54f5bd559487ad6fb3062c8e3a5caf95e25
unknown <altamashbagwan1@gmail.com>
2021-10-27 00:32:26 +0530

    Added: Fuctioning of Enter a First Name

--- src/main/java/com/bl/userRegistration/userRegistration.java
+++ src/main/java/com/bl/userRegistration/userRegistration.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class userRegistration {
 
     public boolean firstName() {
-        boolean matchesString = Pattern.matches("^[A-Z]{1}[a-zA-Z]{3,10}$", "Altamash");
+        boolean matchesString = Pattern.matches("^[A-Z]{1}[a-zA-Z]{3,10}$", "AltamashBagwan");
         return matchesString;
     }
 }
