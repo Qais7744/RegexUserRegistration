407136fb23ede27db5e2e6ed3d940a8d4fb6bbcb
Qais7744 <90202329+Qais7744@users.noreply.github.com>
2021-10-27 03:34:09 +0530

    Update userRegistration.java

--- src/main/java/com/bl/userregistration/userRegistration.java
+++ src/main/java/com/bl/userregistration/userRegistration.java
@@ -2,7 +2,7 @@ package com.bl.userregistration;
 
 import java.util.regex.Pattern;
 
-public class userRegistration {
+public class UserRegistration {
 
     public boolean passwordOneNumeric() {
         boolean matchesString = Pattern.matches("^[A-Z{1}0-9{1}a-z]{8}$", "Al9amash");
