b75585090d5067481f0fe290f467d2ff9573005c
unknown <altamashbagwan1@gmail.com>
2021-10-27 20:09:31 +0530

    Refactored: change logical code UserRegistration class

--- src/main/java/com/bl/userregistration/UserRegistration.java
+++ src/main/java/com/bl/userregistration/UserRegistration.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 
     public boolean passwordSpecialCharacter() {
-        boolean matchesString = Pattern.matches("^([~!@#$%^&*()+{}]?[A-Za-z0-9]+){8,}$", "Altamash&123");
+        boolean matchesString = Pattern.matches("^([~!@#$%^&*()+{}]?[A-Za-z0-9]+){8,}$", "Altamash&7744");
         return matchesString;
     }
 }
